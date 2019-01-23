1d307c50a1220298c708d050ea31353f51eb3140
epgomez <eloasasarus@gmail.com>
2019-01-23 18:29:32 +0100

    just testing

---
+++ session-2/exercise 4.py
@@ -0,0 +1,12 @@
+n: str=input('Enter your chain (If you enter a letter that doesn\'t represent a base, it will be deleted and it will not be co nsidered for the totel lenght): ')
+n = n.lower()
+lenght=len(n)
+for i in n:
+    if not (i=='a' or i=='c' or i=='t' or i=='g'):
+        n=n.replace(i,'')
+a = n.count('a')
+c= n.count('c')
+g= n.count('g')
+t = n.count('t')
+lenght=len(n)
+print('Total lenght: ',lenght,'\nA: ',a,'\nC: ',c,'\nT: ', t,'\nG: ', g)
