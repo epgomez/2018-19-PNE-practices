994bfa2b703521e523b544a47d62abe9a1fd3eec
epgomez <eloasasarus@gmail.com>
2019-02-13 18:15:57 +0100

    13-2-2019, updating P2 and P2-extra and P1

---
+++ P1/Seq_main.py
@@ -0,0 +1,18 @@
+from Seq import Seq
+
+s1 = Seq(input('seq 1: '))
+s2 = Seq(input('seq 2: '))
+s3 = s1.complement()
+s4 = (Seq(s3)).reverse()
+
+seqs = [s1,s2,Seq(s3),Seq(s4)]
+order = [1,2,3,4]
+
+for elem, i in zip(seqs, order):
+    print('Sequence {}: {}'.format(i,elem.strbases.upper()))
+    print('Lenght of the sequence: ', elem.len())
+    bases = ['A', 'C', 'T', 'G']
+    for i in bases:
+        print('\nNumber of {}: {}'.format(i,elem.count(i)))
+        print('Percentage of {}: {}'.format(i,elem.perc(i)))
+    print('\n')
