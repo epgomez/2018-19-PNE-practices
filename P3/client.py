048ab7740b3d292249aebfc7ef18ac870a8e6dd8
epgomez <eloasasarus@gmail.com>
2019-02-27 17:15:46 +0100

    27-2-2019

--- P3/client.py
+++ P3/client.py
@@ -1,22 +1,16 @@
 import socket
 
-port = 8048
-IP = "212.128.253.107"
+port = 8046
+IP = "212.128.253.91"
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((IP, port))
-    msg = input('Please, enter your sequence and the operations you want to perform, separated by commas: ')
-    if msg != '':
-        msg = msg.replace(',', '\n')
-        msg = msg.replace(' ', '')
 
-    else:
-        msg = ' '
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((IP, port))
+msg = ''
 
-    s.send(msg.encode())
+s.send(msg.encode())
 
-    info = s.recv(2048).decode('utf-8')
-    print(info)
+info = s.recv(2048).decode('utf-8')
+print(info)
 
-    s.close()
+s.close()
--- P3/server.py
+++ P3/server.py
@@ -1,20 +1,21 @@
 import socket
 from Seq import Seq
 
-PORT = 8048
-IP = '212.128.253.107'
+PORT = 8046
+IP = '212.128.253.91'
 MAX_CLIENTS = 5
 
 def operations(s, cs):
     """This function makes the operations we are requested to do"""
 
     msg = cs.recv(2048).decode('utf-8')
+    print('message:{}'.format(msg))
     msg = msg.partition('\n')
     seq = msg[0].upper()
     seq = Seq(seq)
     ops = msg[2].split('\n')
 
-    if msg[0] == ' ':
+    if msg[0] == '':
         result = 'ALIVE'
 
     elif not (msg[0].upper().strip('ACTG') == ''):
@@ -31,9 +32,10 @@ def operations(s, cs):
 
         out: str = '{}\nThe sequence is: {}\n'.format(result, seq.strbases)
         for i in ops:
-            for name in methods.keys():
-                if i == name:
-                    out += str(methods[name]) + '\n'
+            if i in methods.keys():
+                out += str(methods[i]) + '\n'
+            else:
+                out += 'Sorry, that is not a registered operation\n'
         return out
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
