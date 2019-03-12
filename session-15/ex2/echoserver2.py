0b886bfd9208a0e97e458ac0848059176b523221
epgomez <eloasasarus@gmail.com>
2019-03-12 16:50:39 +0100

    12-3-2019

--- session-15/ex2/echoserver2.py
+++ session-15/ex2/echoserver2.py
@@ -88,7 +88,7 @@ Handler = TestHandler
 
 # Open the socket server
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Serving at PORT", PORT)
+    print("Serving at POR: ", PORT)
 
     # Main loop: Attend the client. Whenever there is a new
     # clint, the handler is called
